fea50e597ad697bb359f4ccc25df5269dfebc7b3
jaeukryu <jaeukryu8@gmail.com>
2023-04-04 09:41:24 +0900

    change plt show

--- graphs.py
+++ graphs.py
@@ -2,10 +2,23 @@ import matplotlib.pyplot as plt
 from crack_cnn import history
 
 # Plot the accuracy
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('Model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epoch')
-plt.legend(['Train', 'Validation'], loc='upper left')
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1,len(acc) + 1)
+
+plt.plot(epochs , acc , 'bo', label = 'Training acc')
+plt.plot(epochs, val_acc, 'b', label = 'Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'bo', label = 'Training loss')
+plt.plot(epochs, val_loss, ' b', label = 'Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
 plt.show()
